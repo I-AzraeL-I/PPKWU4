6e5e873376fb2365b3ba591a949802bf760d6eba
I-AzraeL-I <qaraqan70@gmail.com>
2021-11-25 12:44:04 +0100

    First commit

---
+++ src/main/java/com/mycompany/stringapiwrapper2/StringApiWrapper2Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.stringapiwrapper2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringApiWrapper2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringApiWrapper2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/stringapiwrapper2/StringApiWrapper2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.stringapiwrapper2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringApiWrapper2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
